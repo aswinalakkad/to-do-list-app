84b4360fd6c8f9a5b5b3a0109b27716eb4da7ed9
aswinalakkad <aswinalakkad00@gmail.com>
2025-08-21 17:34:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,11 +105,15 @@ else:
 
             # Task content
             with col2:
+                title_display = (
+                    f"<s>{task['title']}</s>" if task["completed"] else f"{task['title']}"
+                )
+
                 st.markdown(
                     f"""
                     <div style='padding:12px; border:1px solid #e5e7eb; border-radius:12px; margin-bottom:8px; background:#f9fafb;'>
-                        <b>{task['title']}</b>
-                        <span style='background-color:#0f172a; color:white; padding:2px 8px; border-radius:8px; font-size:12px;'>
+                        <b>{title_display}</b>
+                        <span style='background-color:#6b7280; color:white; padding:2px 8px; border-radius:8px; font-size:12px;'>
                             {task['priority'].upper()}
                         </span>
                         📅 {task['due']}
